131654150e9a9c09abfa379c69b01330f230494f
LauraIu <iuga.laura.katalin.com>
2022-12-08 18:13:51 +0100

    Updates

---
+++ precache-manifest.9e774e2338b85f71010ea81fde608fe0.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = [
+  {
+    "revision": "d36012bde970a496e1f0",
+    "url": "/book-a-table/static/css/main.ed431326.chunk.css"
+  },
+  {
+    "revision": "d36012bde970a496e1f0",
+    "url": "/book-a-table/static/js/main.d36012bd.chunk.js"
+  },
+  {
+    "revision": "a3e25b2327350f3d59ad",
+    "url": "/book-a-table/static/css/1.c5282ea7.chunk.css"
+  },
+  {
+    "revision": "a3e25b2327350f3d59ad",
+    "url": "/book-a-table/static/js/1.a3e25b23.chunk.js"
+  },
+  {
+    "revision": "11c44e84a70cf4361954",
+    "url": "/book-a-table/static/js/runtime~main.11c44e84.js"
+  },
+  {
+    "revision": "11e105ca5e7ae74cdf74bef371f44ff0",
+    "url": "/book-a-table/static/media/product1.11e105ca.jpg"
+  },
+  {
+    "revision": "d59c1ef5c9ebb0ec07a24e41dff32a1e",
+    "url": "/book-a-table/static/media/product2.d59c1ef5.jpg"
+  },
+  {
+    "revision": "a2db9a88552dcf5f0f50aefec521eb48",
+    "url": "/book-a-table/static/media/pizza_heroBg_96dpi_1920.a2db9a88.jpeg"
+  },
+  {
+    "revision": "cc132196cfad360741ae927f0775f353",
+    "url": "/book-a-table/static/media/MammaMia_interior.cc132196.jpeg"
+  },
+  {
+    "revision": "de1ca1aaedf902ea0875b372c6263708",
+    "url": "/book-a-table/static/media/product3.de1ca1aa.jpg"
+  },
+  {
+    "revision": "469eb37e3588c7f2901fc5bf82d251b1",
+    "url": "/book-a-table/static/media/product4.469eb37e.jpg"
+  },
+  {
+    "revision": "fa2772327f55d8198301fdb8bcfc8158",
+    "url": "/book-a-table/static/media/glyphicons-halflings-regular.fa277232.woff"
+  },
+  {
+    "revision": "e18bbf611f2a2e43afc071aa2f4e1512",
+    "url": "/book-a-table/static/media/glyphicons-halflings-regular.e18bbf61.ttf"
+  },
+  {
+    "revision": "448c34a56d699c29117adc64c43affeb",
+    "url": "/book-a-table/static/media/glyphicons-halflings-regular.448c34a5.woff2"
+  },
+  {
+    "revision": "89889688147bd7575d6327160d64e760",
+    "url": "/book-a-table/static/media/glyphicons-halflings-regular.89889688.svg"
+  },
+  {
+    "revision": "f4769f9bdb7466be65088239c12046d1",
+    "url": "/book-a-table/static/media/glyphicons-halflings-regular.f4769f9b.eot"
+  },
+  {
+    "revision": "5afc12afba1694c9f5826008c010a6c6",
+    "url": "/book-a-table/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/portfolio/precache-manifest.30a24e94a0e0531ba5c3d834ae85ea4b.js"
+  "/book-a-table/precache-manifest.9e774e2338b85f71010ea81fde608fe0.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/portfolio/index.html", {
+workbox.routing.registerNavigationRoute("/book-a-table/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
